16ff6a0d61016a2caf3c592e7dfd65a8de1ec3c4
MinhTran0220 <minhngocny@hotmail.com>
2019-05-03 02:43:16 -0400

    Fixing Array Pictures

--- client/src/components/AppointmentArtistCards/index.js
+++ client/src/components/AppointmentArtistCards/index.js
@@ -5,54 +5,68 @@ import StarRating from "../StarRating/index";
 import "./style.css";
 
 class ArtistCard extends React.Component {
-    constructor(props){
+    constructor(props) {
         super(props)
 
         this.state = {
-            images:images
+            images: images
         }
     }
 
-     
-  render() {
-    return (
-        <span>
-      <div className="artistCardWrapper">
-      {this.state.images.map(image => (
-        <div className="card">
-      <div className="img-container">
-        <img alt={image.alt} src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/27/Square_200x200.svg/200px-Square_200x200.svg.png" />
-       
-      </div>
-      <div className="content">
-        <ul>
-          <li>
-            <strong>Artist Name: </strong> {image.name}
-          </li>
-          <li>
-            <strong>Specialties</strong> {image.occupation}
-          </li>
-          <li>
-            <strong>Rating</strong> <StarRating />
-          </li>
-          <li>
-           
-            <Button variant="danger">
-              View Artist
-            </Button>
-          </li>
-        </ul>
-      </div>
-     {/*} <span onClick={() => props.removeFriend(props.id)} className="remove">
-        𝘅
-    </span> */}
-    </div>
-      ))}
-      </div>
 
-      
-    )
-  }
+    render() {
+        return ( <
+            span >
+            <
+            div className = "artistCardWrapper" > {
+                this.state.images.map(image => ( <
+                    div className = "card" >
+                    <
+                    div className = "img-container" >
+                    <
+                    img alt = { image.alt }
+                    src = "https://upload.wikimedia.org/wikipedia/commons/thumb/2/27/Square_200x200.svg/200px-Square_200x200.svg.png" / >
+
+                    <
+                    /div> <
+                    div className = "content" >
+                    <
+                    ul >
+                    <
+                    li >
+                    <
+                    strong > Artist Name: < /strong> {image.name} <
+                    /li> <
+                    li >
+                    <
+                    strong > Specialties < /strong> {image.occupation} <
+                    /li> <
+                    li >
+                    <
+                    strong > Rating < /strong> <StarRating / >
+                    <
+                    /li> <
+                    li >
+
+                    <
+                    Button variant = "danger" >
+                    View Artist <
+                    /Button> <
+                    /li> <
+                    /ul> <
+                    /div> {
+                        /*} <span onClick={() => props.removeFriend(props.id)} className="remove">
+                                𝘅
+                            </span> */
+                    } <
+                    /div>
+                ))
+            } <
+            /div>
+
+
+        )
+    }
 }
 
 export default ArtistCard;
